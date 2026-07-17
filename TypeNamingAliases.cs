21b4e380c95b321d0b61bf0a44d995482bbb0b48
Min Wan <minwan@microsoft.com>
2026-07-17 21:39:17 +0800

    Unify effect alias naming to EdgeEffect
    Remove transitional aliases and keep a single naming convention: DisplayEdgeEffect and SearchEdgeEffect.

--- TypeNamingAliases.cs
+++ TypeNamingAliases.cs
@@ -1,10 +1,10 @@
 global using DisplayTree = StrategyPlan;
 global using DisplayNode = StrategyNode;
 global using DisplayEdge = StrategyBranch;
-global using DisplayTransitionEffect = StrategyEffect;
+global using DisplayEdgeEffect = StrategyEffect;
 global using EdgeEquivalenceSummary = EquivalentOrderSummary;
 
 global using SearchTree = SearchStrategy;
 global using SearchTreeNode = SearchNode;
 global using SearchEdge = SearchBranch;
-global using SearchTransition = SearchEffect;
+global using SearchEdgeEffect = SearchEffect;
